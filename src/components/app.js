f3cbc1b621da6a9976a3dab2e44edbd353be7843
Zack Bostian <bostianz@gmail.com>
2016-02-12 22:50:00 -0500

    set up bindActionCreators in app clas container

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+//import actions
+import { UpdateText } from '../reducers/reducer_todos';
+import { AddToDo } from '../reducers/reducer_todos';
+
+//connect to store
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'react-redux';
 import List from '../containers/List';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
   }
   updateText(e) {
-    store.dispatch(UpdateText(e.target.value));
-    console.log(store);
-  }
-  addListItem(e) {
-    store.dispatch(AddToDo(text))
-    console.log('test');
+    this.props.UpdateText(e.target.value);
+    console.log(value);
   }
 
   render() {
@@ -20,11 +23,17 @@ class App extends React.Component {
         <h1>To Do App</h1>
         <List
           updateText={this.updateText.bind(this)}
-          addListItem={this.addListItem.bind(this)}
           />
       </div>
     )
   }
 }
 
-export default App;
+
+let mapDispatchToProps = (dispatch) => {
+  //takes single action or object as first arg
+  //takes dispatch as second arg
+  return bindActionCreators({AddToDo: AddToDo, UpdateText: UpdateText}, dispatch);
+}
+
+let MyConnectedClass = connect(null, mapDispatchToProps)(App);
