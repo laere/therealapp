2c839f1e72f2f4e642874da40dcd9521b7ada630
Zack Bostian <bostianz@gmail.com>
2016-02-18 09:29:43 -0500

    made small changes to AddTodo AC

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/app';
 
 import { Provider } from 'react-redux';
 import { TodoState } from '../src/reducers/reducer_todos';
-//middleware provides a extension pt between dispatching an action
+//middleware provides a extension point between dispatching an action
 //and the moment it reaches the reducer (hence: MIDDLEware)
 import { createStore, applyMiddleware } from 'redux';
 import Promise from 'redux-promise';
--- src/reducers/reducer_todos.js
+++ src/reducers/reducer_todos.js
@@ -1,6 +1,9 @@
+
 import dotProp from 'dot-prop-immutable';
 import axios from 'axios';
 
+//location of the todos API
+const API = 'http://localhost:3000/todo/';
 //save string values to vars
 const ADD_TODO = 'ADD_TODO';
 const REMOVE_TODO = 'REMOVE_TODO';
@@ -9,42 +12,48 @@ const FETCH_TODOS = 'FETCH_TODOS';
 
 let nextId = 0;
 let index = 0;
-//the action is whats performed to alter state
-//addItem is an action creator and nees to return an action
+
+//addItem is an action creator and needs to return an action
+//actions are plain javascript objects
 export const AddTodo = (text) => {
 
-  const post = axios.post('http://localhost:3000/todo/', {text});
-
+  const post = axios.post(API, { text });
+  //logging purposes
   post.then(function(response) {
-    }, function(err) {
-      console.log("Error performing action!");
+    if(response)
+      return console.log(response);
+  }, function(err) {
+    if(err)
+      return console.log('Error posting data...');
   });
-  return {
-    type: ADD_TODO,
-    id: nextId++,
-    payload: text
-  }
+
+    return {
+      type: ADD_TODO,
+      id: nextId++,
+      payload: text
+    }
 }
 
 export const RemoveTodo = (id) => {
+  const remove = axios.delete(API + id);
   return {
     type: REMOVE_TODO,
     id
   };
 }
-
-export const FetchTodos = (id) => {
-  const req = axios.get('http://localhost:3000/todo/');
+//grab todos on load
+export const FetchTodos = () => {
+  const req = axios.get(API);
   return {
     type: FETCH_TODOS,
     payload: req
   };
 }
 
-//initial state of items and text
 const initialState = {
   items: [],
 }
+
 //A pure function that takes the current/prev state
 //and an action, and returns the next state
 //Reducer controls the state, and is where state lives.
@@ -52,7 +61,7 @@ export const TodoState = (state = initialState, action) => {
   switch(action.type) {
     case ADD_TODO:
       state = dotProp.set(state, 'items', items => [...items, { text: action.payload }])
-      console.log('This is my state after ADD_TODO : ' + state, action);
+      console.log(state, action);
       return state;
     case REMOVE_TODO:
       state = dotProp.delete(state, `items.${index}`);
